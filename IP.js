7c77c4462e442a2562a82b40bfe3745b23133a31
sj-10ps <sps24723@gmail.com>
2025-08-31 01:30:04 +0530

    minor changes

--- IP.js
+++ IP.js
@@ -1,5 +1,7 @@
 import Constants from "expo-constants"
 const IP=Constants.expoConfig.extra.API_URL
 
-// const IP=`http://<Your-IPV4-Address>:4000`
+//comment out the first ip and use your IP for local machine
+//const IP=`http://<Your-IPV4-Address>:4000`
+
 export default IP
